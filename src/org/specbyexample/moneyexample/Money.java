303b4df5a0ceb26cc4d3e4cf2a83efc14d01eb77
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2013-07-18 22:41:06 +0200

    12: Interesting times

--- src/org/specbyexample/moneyexample/Money.java
+++ src/org/specbyexample/moneyexample/Money.java
@@ -3,19 +3,29 @@ package org.specbyexample.moneyexample;
 public abstract class Money {
 
     protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
 
     public static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
-    
+
     public static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
     }
 
     @Override
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount && getClass().equals(money.getClass());
+    }
+
+    public String getCurrency() {
+        return currency;
     }
 
     public abstract Money times(int amount);
